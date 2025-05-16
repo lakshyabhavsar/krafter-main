473b104df6dd3b43e1af418b14a07fa9634eeedc
lakshyabhavsar <lakshyabhavsr@gmail.com>
2025-05-16 11:30:58 +0530

    better1

--- src/app/HomePage/hero/hero-parallax.tsx
+++ src/app/HomePage/hero/hero-parallax.tsx
@@ -52,7 +52,7 @@ export const HeroParallax = ({
     springConfig
   );
   const translateY = useSpring(
-    useTransform(scrollYProgress, [0, 0.3], [-500, 200]),
+    useTransform(scrollYProgress, [0, 0.3], [-400, 100]),
     springConfig
   );
   return (
